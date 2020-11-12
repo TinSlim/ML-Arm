91e5c6ccfeb6b359289651bb5abbca667ebb0dee
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-11-12 00:17:28 -0300

    Fixed rotations.

--- controller.py
+++ controller.py
@@ -1,10 +1,27 @@
 import glfw
+import sys
 
-class controller():
-    
+class Controller():
+
+    def __init__(self):
+        self.camera_angle = 0
+        self.dt = 1
+
+    def rotate_camera_angle(self,x):
+        self.camera_angle += x
+
+    def set_dt(self,new_dt):
+        self.dt = new_dt     
+            
     def on_key(self,window, key, scancode, action, mods):
-        if action != glfw.PRESS:
-            return
-    
+        if key == glfw.KEY_LEFT:
+            self.rotate_camera_angle(-2 * self.dt)
+        
+        elif key == glfw.KEY_RIGHT:
+            self.rotate_camera_angle(2 * self.dt)
+
         elif key == glfw.KEY_ESCAPE:
             sys.exit()
+
+        else:#action != glfw.PRESS:
+            return
