f5781970798db79f8b982b17ae1ceb3b12df80ad
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-11-12 00:55:25 -0300

    Added methods to rotate.

--- arm.py
+++ arm.py
@@ -1,100 +1,54 @@
+import Modulo.transformations as tr
+import Modulo.basic_shapes as bs
+import Modulo.scene_graph as sg
+import Modulo.easy_shaders as es
+import Modulo.readobj as rbj
+import Modulo.lighting_shaders as ls
 
 class Arm:
 
     def __init__(self):
-        self.rotation = 0
-        self.angle_1 = 0
-        self.angle_2 = 0
-    
-    def rotation(self,add_rotation,add_angle_1,add_angle_2):
+        self.arm0_x = 0
+        self.arm0_y = 0
+        self.arm0_z = 0
+
+    def create_parts(self):
+        gpu_base = es.toGPUShape(bs.createColorNormalsCube(1, 0, 0))
+        gpu_arm0 = es.toGPUShape(bs.createColorNormalsCube(0, 1, 0))
+        gpu_arm1 = es.toGPUShape(bs.createColorNormalsCube(0, 0, 1))
         
+        node_arm1 = sg.SceneGraphNode("arm1")
+        node_arm1.transform = tr.scale(1,1,3)#tr.matmul([tr.scale(1,1,3),tr.translate(0,0,0)]) 
+        node_arm1.childs += [gpu_arm1]
+        
+        node_arm0 = sg.SceneGraphNode("arm0")
+        node_arm0.transform = tr.scale(1,1,3)#tr.matmul([tr.scale(1,1,3),tr.identity()]) 
+        node_arm0.childs += [gpu_arm0]
 
+        node_arms1 = sg.SceneGraphNode("arms1")
+        node_arms1.transform = tr.translate(0,0,3)
+        node_arms1.childs += [node_arm1]
 
+        node_arms0 = sg.SceneGraphNode("arms0")
+        node_arms0.transform = tr.matmul([tr.rotationX(self.arm0_x),tr.translate(0,0,2)])
+        node_arms0.childs += [node_arm0,node_arms1]
 
-
-def pajaro():
-    gpuCabeza = es.toGPUShape(rbj.readOBJ("Model/cabeza.obj", (0,1,0.8)))
-    gpuTorso = es.toGPUShape(rbj.readOBJ("Model/torso.obj", (0,0.8,1)))
-    gpuAlaSupDer = es.toGPUShape(rbj.readOBJ("Model/alasupder.obj", (0,1,1)))
-    gpuAlaInfDer = es.toGPUShape(rbj.readOBJ("Model/alainfder.obj", (0,0.7,1)))
-    gpuAlaSupIzq = es.toGPUShape(rbj.readOBJ("Model/alasupizq.obj", (0,1,1)))
-    gpuAlaInfIzq = es.toGPUShape(rbj.readOBJ("Model/alainfizq.obj", (0,0.7,1)))
-
-    #################Ala Derecha
-    #Ala inf derecha
-    AlaInfDerecha = sg.SceneGraphNode("AlaInfDerecha")
-    AlaInfDerecha.transform = tr.translate(0,0,0)  #Ajustar bn
-    AlaInfDerecha.childs+=[gpuAlaInfDer]
-
-    #Rotación ala inf derecha
-    RotacionAlaInfDerecha = sg.SceneGraphNode("RotacionAlaInfDerecha")
-    RotacionAlaInfDerecha.transform = tr.matmul([tr.translate(-1.5,-2,-2),tr.rotationY(np.pi/3),tr.translate(1,2,2)])
-    RotacionAlaInfDerecha.childs+= [AlaInfDerecha]
-
-    #Ala sup derecha
-    AlaSupDerecha = sg.SceneGraphNode("AlaSupDerecha")
-    AlaSupDerecha.childs+=[gpuAlaSupDer]
-   
-    #Ala derecha
-    AlaDerecha = sg.SceneGraphNode("AlaDerecha")
-    AlaDerecha.childs+=[AlaSupDerecha]
-    AlaDerecha.childs+=[RotacionAlaInfDerecha]
-    AlaDerecha.transform = tr.matmul([tr.translate(-1,-2.5,0),tr.rotationY(np.pi),tr.rotationX(np.pi),tr.translate(1,2.5,0)])  #AlaDerecha.transform = tr.matmul([tr.translate(0,-2,0),tr.rotationX(np.pi*(2/3)),tr.translate(0,2,0.5)])#
-
-    ################# Ala Izquierda
-    #Ala inf izquierda
-    AlaInfIzquierda = sg.SceneGraphNode("AlaInfIzquierda")
-    AlaInfIzquierda.transform = tr.translate(0,0,0)  #Ajustar bn
-    AlaInfIzquierda.childs+=[gpuAlaInfIzq]
-
-    #Rotación ala inf izquierda
-    RotacionAlaInfIzquierda = sg.SceneGraphNode("RotacionAlaInfIzquierda") 
-    RotacionAlaInfIzquierda.transform = tr.matmul([tr.translate(1.5,-2,-2),tr.rotationY(-np.pi/3),tr.translate(-1,2,2)])
-    RotacionAlaInfIzquierda.childs+= [AlaInfIzquierda]
-
-    #Ala sup izquierda
-    AlaSupIzquierda = sg.SceneGraphNode("AlaSupIzquierda")
-    AlaSupIzquierda.childs+=[gpuAlaSupIzq]
-
-    #Ala izquierda
-    AlaIzquierda = sg.SceneGraphNode("AlaIzquierda")
-    AlaIzquierda.childs+=[AlaSupIzquierda]
-    AlaIzquierda.childs+=[RotacionAlaInfIzquierda]
+        node_base = sg.SceneGraphNode("base")
+        node_base.transform = tr.scale(3,3,1) 
+        node_base.childs += [gpu_base]
+        
+        node_all = sg.SceneGraphNode("all")
+        node_all.childs += [node_arms0, node_base]
+        
+        self.big_node = node_all
     
-    ################# Ambas Alas
-    #Alas
-    Alas = sg.SceneGraphNode("Alas")
-    Alas.childs+=[AlaDerecha]
-    Alas.childs+=[AlaIzquierda]
+    def rotate_arm0(self,value,value2):
+        arm0 = sg.findNode(self.big_node, "arms0")
+        arm0.transform = tr.matmul([tr.rotationZ(value),tr.rotationX(value2),tr.translate(0,0,2)])
 
-    ################# Cuerpo
-    #Torso
-    Torso = sg.SceneGraphNode("Torso")
-    Torso.childs+= [gpuTorso]
-    #Torso.transform=tr.matmul([tr.rotationX(np.pi/2),tr.uniformScale(1)])
-    #Torso.transform=tr.matmul([tr.rotationZ(90),tr.translate(0,0,0)])
 
-    #Cabeza
-    Cabeza = sg.SceneGraphNode("Cabeza")
-    Cabeza.childs+= [gpuCabeza]
-    #Cabeza.transform=tr.rotationX(np.pi/2)
 
-    #Cuerpo
-    Cuerpo = sg.SceneGraphNode("Cuerpo")
-    Cuerpo.childs+= [Torso]
-    Cuerpo.childs+= [Cabeza]
 
-    ################# Pájaro
-    #Pajaro Total1
-    Pajaro1 = sg.SceneGraphNode("Pajaro1")
-    Pajaro1.childs+=[Alas]
-    Pajaro1.childs+=[Cuerpo]
-    Pajaro1.transform=tr.matmul([tr.uniformScale(0.1),tr.rotationX(np.pi*(1/2))])
-
-    #Pajaro
-    Pajaro = sg.SceneGraphNode("Pajaro")
-    Pajaro.childs+=[Pajaro1]
-    return Pajaro
 
 
 
