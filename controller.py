5b5a0f9b961c015a184156ff0ca34ec558637c85
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-11-12 23:04:28 -0300

    Now the arm1 can be moved.

--- controller.py
+++ controller.py
@@ -6,8 +6,9 @@ class Controller():
     def __init__(self):
         self.camera_angle = 0
         self.dt = 1
-        self.angle = 0
-        self.rotation = 0
+        self.angle0 = 0
+        self.rotation0 = 0
+        self.rotation1 = 0
 
     def rotate_camera_angle(self,x):
         self.camera_angle += x
@@ -23,14 +24,19 @@ class Controller():
             self.rotate_camera_angle(2 * self.dt)
 
         elif key == glfw.KEY_A:
-            self.angle += 0.2
+            self.angle0 += 0.05
         elif key == glfw.KEY_S:
-            self.angle -= 0.2
+            self.angle0 -= 0.05
 
         elif key == glfw.KEY_D:
-            self.rotation += 0.2
+            self.rotation0 += 0.05
         elif key == glfw.KEY_F:
-            self.rotation -= 0.2
+            self.rotation0 -= 0.05
+
+        elif key == glfw.KEY_Z:
+            self.rotation1 += 0.05
+        elif key == glfw.KEY_X:
+            self.rotation1 -= 0.05
 
         elif key == glfw.KEY_ESCAPE:
             sys.exit()
