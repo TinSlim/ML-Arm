9f402e2e0c77e7ad884a8b3107e6ea433937222a
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-11-12 23:16:17 -0300

    Controller controls the arm.

--- controller.py
+++ controller.py
@@ -3,12 +3,13 @@ import sys
 
 class Controller():
     
-    def __init__(self):
+    def __init__(self, arm):
         self.camera_angle = 0
         self.dt = 1
         self.angle0 = 0
         self.rotation0 = 0
         self.rotation1 = 0
+        self.arm = arm
 
     def rotate_camera_angle(self,x):
         self.camera_angle += x
@@ -24,19 +25,19 @@ class Controller():
             self.rotate_camera_angle(2 * self.dt)
 
         elif key == glfw.KEY_A:
-            self.angle0 += 0.05
+            self.arm.add_to_angle(0.05)
         elif key == glfw.KEY_S:
-            self.angle0 -= 0.05
+            self.arm.add_to_angle(-0.05)
 
         elif key == glfw.KEY_D:
-            self.rotation0 += 0.05
+            self.arm.add_to_arm0_rotation(0.05)
         elif key == glfw.KEY_F:
-            self.rotation0 -= 0.05
+            self.arm.add_to_arm0_rotation(-0.05)
 
         elif key == glfw.KEY_Z:
-            self.rotation1 += 0.05
+            self.arm.add_to_arm1_rotation(0.05)
         elif key == glfw.KEY_X:
-            self.rotation1 -= 0.05
+            self.arm.add_to_arm1_rotation(-0.05)
 
         elif key == glfw.KEY_ESCAPE:
             sys.exit()
