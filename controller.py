4e503a3eb7db53cc974d4e9df2891389ae508346
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-11-15 01:25:46 -0300

    Added auto move with P.

--- controller.py
+++ controller.py
@@ -3,22 +3,63 @@ import sys
 
 class Controller():
     
-    def __init__(self, arm):
+    def __init__(self, arm, clf):
         self.camera_angle = 0
         self.dt = 1
         self.angle0 = 0
         self.rotation0 = 0
         self.rotation1 = 0
         self.arm = arm
+        self.clf = clf
 
     def rotate_camera_angle(self,x):
         self.camera_angle += x
 
     def set_dt(self,new_dt):
         self.dt = new_dt     
-            
+    
+    def auto_action(self,r0_r1_ang):
+        if r0_r1_ang[0]:
+            self.arm.add_to_arm0_rotation(0.05)
+        else:
+            self.arm.add_to_arm0_rotation(-0.05)
+
+        if r0_r1_ang[1]:
+            self.arm.add_to_arm1_rotation(0.05)
+        else:
+            self.arm.add_to_arm1_rotation(-0.05)
+
+        if r0_r1_ang[2]:
+            self.arm.add_to_angle(0.05)
+        else:
+            self.arm.add_to_angle(-0.05)
+        
     def on_key(self,window, key, scancode, action, mods):
-        if key == glfw.KEY_LEFT:
+        if key == glfw.KEY_P:
+            x = self.arm.ball.x
+            y = self.arm.ball.y
+            z = self.arm.ball.z
+            angle = self.arm.angle
+            rot0 = self.arm.arm0_rotation
+            rot1 = self.arm.arm1_rotation
+
+            prediction = self.clf.predict([[x,y,z,angle,rot0,rot1]])[0]
+            print(prediction)
+            switcher = {
+                0: [True,True,True],
+                1: [False,True,True],
+                2: [False,False,True],
+                3: [True,False,True],
+                4: [True,True,False],
+                5: [False,True,False],
+                6: [False,False,False],
+                7: [True,False,False]
+            }
+            self.auto_action(switcher[prediction])
+
+
+
+        elif key == glfw.KEY_LEFT:
             self.rotate_camera_angle(-2 * self.dt)
         
         elif key == glfw.KEY_RIGHT:
