14bf0334a729d26b09e29477746bd06562172c03
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-11-15 01:26:00 -0300

    Classifier class.

--- classifier.py
+++ classifier.py
@@ -1,18 +1,18 @@
 from sklearn import svm
 import pandas as pd
 
-class classifier:
+class Classifier:
     def __init__(self,file_name):
         self.file_name = file_name
         self.clf = svm.SVC()
 
     def get_data(self):
         self.data = pd.read_csv(self.file_name,index_col=False)
-        self.classes = data.iloc[:,6]
-        self.attributes = data.iloc[:,:6]
+        self.classes = self.data.iloc[:,6]
+        self.attributes = self.data.iloc[:,:6]
     
     def fit(self):
         self.clf.fit(self.attributes.values,self.classes)
     
-    def predict(self,value)
+    def predict(self,value):
         return self.clf.predict(value)
